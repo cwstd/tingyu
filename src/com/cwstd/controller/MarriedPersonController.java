2906e0634847cd52f0cff648c521885cc4033ad0
cwstd <chenweistd@163.com>
2021-09-23 01:04:55 +0800

    还差阿里云短信验证功能

--- src/com/cwstd/controller/MarriedPersonController.java
+++ src/com/cwstd/controller/MarriedPersonController.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cwstd.pojo.Host;
+import com.cwstd.pojo.HostResult;
 import com.cwstd.pojo.MarriedPerson;
 import com.cwstd.pojo.PageResult;
 import com.cwstd.service.IMarriedPersonService;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -34,6 +38,27 @@ public class MarriedPersonController {
         PageResult<MarriedPerson> marriedPersonPageResult = iMarriedPersonService.selectAll(page, rows, pname, phone);
         return marriedPersonPageResult;
     }
+    @ResponseBody
+    @RequestMapping("personLogin")
+    public MarriedPerson personLogin(String phone,String ppwd ){
+        QueryWrapper<MarriedPerson> queryWrapper = new QueryWrapper();
+        queryWrapper.eq("phone",phone);
+        queryWrapper.eq("ppwd",ppwd);
+        MarriedPerson one = iMarriedPersonService.getOne(queryWrapper);
+        return one;
+    }
 
+    @ResponseBody
+    @RequestMapping("reg")
+    public HostResult reg(MarriedPerson marriedPerson){
+        HostResult hostResult = new HostResult();
+        marriedPerson.setRegdate(LocalDateTime.now());
+        marriedPerson.setMarrydate(LocalDate.now());
+        marriedPerson.setStatus("1");
+        boolean insert = marriedPerson.insert();
+        hostResult.setSuccess(insert);
+        hostResult.setMsg("成功！");
+        return hostResult;
+    }
 }
 
